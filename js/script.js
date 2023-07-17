0e2b23ba33a9f9d9afddb7935bd046d56847ad6f
GAB3theGR8 <120361003+GAB3theGR8@users.noreply.github.com>
2023-07-16 20:26:06 -0400

    responsive design changes / animations

--- js/script.js
+++ js/script.js
@@ -23,18 +23,27 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+
+            sec.classList.add('show-animate');
+        }
+        
+        else {
+            sec.classList.remove('show-animate');
         }
     });
+
     // sticky header
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    
-
     // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
 
     // animation footer on scroll
+    let footer = document.querySelector('footer');
 
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
